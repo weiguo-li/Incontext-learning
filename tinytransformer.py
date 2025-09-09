050c076a3b08a82c77cfe5cae622a03becb9836f
Weiguo Li <lwg_love_kobe24@163.com>
2025-09-09 13:43:57 +0200

    linear attention abstract

---
+++ tinytransformer.py
@@ -0,0 +1,25 @@
+import torch.nn as nn
+
+
+
+# use the Jax to make code much simple and clear to read
+
+class LinearRelaxedAttention(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def forward(self,x):
+        pass
+
+
+class LinearNanoGPT(nn.Module):
+    def __init__(self) -> None:
+        """
+        to initialize a small GPT but with relaxed linear attention
+        """
+        super().__init__()
+
+    def forward(self, x):
+        pass
+
+    
